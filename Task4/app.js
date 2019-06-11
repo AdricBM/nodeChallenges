4b797741777f2e05bf5989b0354b03aa5bbc39d1
Adric1 <aplanktin@gmail.com>
2019-06-11 17:21:14 +0200

    Task4 1st commit

--- Task4/app.js
+++ Task4/app.js
@@ -1,42 +1,84 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv/config');
 
-/**
- * import routes
- */
-app.use(bodyParser.json());
+// Connect to DB
+mongoose.connect('mongodb://localhost/registration',{ useNewUrlParser: true });
+let db = mongoose.connection;
 
-const prospectRoutes = require('./routes/prospects');
-
-/**
- * Middleware
- */
-
- app.use('/prospects',prospectRoutes);
- 
-
-/**
- * Routes
- */
-
-app.get('/', (req,res) =>
-{
-    res.send("we are on home");
+// Check DB Connection
+db.once('open',()=>{
+    console.log('connected to MongoDB');
 });
 
-/**
- * connet to database
- */
+// Check for DB errors
+db.on('error',(err)=>{
+    console.log(err);
+});
 
-mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true },
-    ()=>console.log('connected to DB'));
+// Init App
+const app = express();
 
-/**
- * listen to port
- */
-app.listen(3000);
+// Bring in models
+let Prospect = require('./models/prospect');
+
+// Load view engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+// Body-parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set public folder
+app.use(express.static(path.join(__dirname,'public'
+)));
+
+// Home Route
+app.get('/',(req,res)=>{
+    Prospect.find({},(err,prospects)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render('index',{
+                title: 'Prospects',
+                prospects: prospects
+            }); 
+        }
+        
+    });
+});
+
+// Add route
+app.get('/prospects/add',(req,res)=>{
+    res.render('add_prospect',{
+        title: 'Add Prospect'
+    });
+});
+
+// Add submit POST route
+app.post('/prospects/add',(req,res)=>{
+    let prospect = new Prospect();
+    prospect.name = req.body.name;
+    prospect.surname = req.body.surname;
+    prospect.age = req.body.age;
+    prospect.soi = req.body.soi;
+    prospect.assistant = req.body.assistant;
+
+    prospect.save((err)=>{
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+// Start Server
+app.listen(3000,()=>{
+    console.log('Server started on port 3000');
+});
---
+++ Task4/models/prospect.js
@@ -0,0 +1,33 @@
+let mongoose = require('mongoose');
+
+// Prospect Schema
+let prospectSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    surname:{
+        type: String,
+        required: true
+    },
+    age:{
+        type: Number,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now(),
+        required: true
+    },
+    soi:{
+        type: String,
+        required: true
+    },
+    assistant:{
+        type: String,
+        required: true
+    }
+});
+
+let Prospect = module.exports = mongoose.model('Prospect',prospectSchema);
+
